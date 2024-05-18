681e2eee83e6c06fbe529100411d36c5cbe8ec0e
Gonzalo <gonzalodiez97@gmail.com>
2024-05-18 16:44:42 -0300

    Entrega para desafio

--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -1,6 +1,6 @@
 const socket = io.connect('http://localhost:8080');
 const token = localStorage.getItem('token');
-const userRole = localStorage.getItem('userRole')
+const userRole = localStorage.getItem('userRole');
 
 socket.on('addMessage', (addMessage) => {
     const chatList = document.getElementById('chatList');
@@ -8,7 +8,7 @@ socket.on('addMessage', (addMessage) => {
     chatElement.classList.add('col-md-4', 'mb-4');
     chatElement.innerHTML = `
         <div class="card">
-            <h2>usuario: ${addMessage.user.email}</h2>
+            <h2>usuario: ${addMessage.userEmail}</h2>
             <p>mensaje: ${addMessage.text}</p>
         </div>`;
     chatList.appendChild(chatElement);
@@ -17,10 +17,10 @@ socket.on('addMessage', (addMessage) => {
 document.getElementById('messageForm').addEventListener('submit', async (event) => {
     event.preventDefault();
 
-    const user = document.getElementById('user').value;
+    const userEmail = document.getElementById('user').value;
     const message = document.getElementById('message').value;
 
-    if (user && message) {
+    if (userEmail && message) {
         try {
             const response = await fetch('http://localhost:8080/api/messages/addMessage', {
                 method: 'POST',
@@ -28,15 +28,15 @@ document.getElementById('messageForm').addEventListener('submit', async (event) 
                     'Content-Type': 'application/json',
                     "authorization": `Bearer ${token}`
                 },
-                body: JSON.stringify({ user, text: message })
+                body: JSON.stringify({ userEmail, text: message })
             });
 
             if (!response.ok) {
                 throw new Error('Error al agregar el mensaje');
             }
 
-            console.log("Mensaje agregado:", { user: user, message });
-            socket.emit("addMessage", { user: user, text: message });
+            console.log("Mensaje agregado:", { userEmail, message });
+            socket.emit("addMessage", { userEmail, text: message });
             document.getElementById('message').value = '';
 
             event.target.reset();
